202a33b282d0ad6f1e1d352eaac50e3fd24f34f0
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-12 22:35:13 +0000

    Change if statements to guards in game.rb

--- models/game.rb
+++ models/game.rb
@@ -3,29 +3,24 @@ class Game
   def initialize(hand1, hand2)
     @hand1 = hand1
     @hand2 = hand2
+    # @players = [ @hand1, @hand2 ]
   end
 
   def play
+    # @players.rotate()
+    result1 = "SCISSORS beats PAPER!"
+    result2 = "PAPER beats ROCK!"
+    result3 = "ROCK beats SCISSORS!"
 
-    if @hand1 == "scissors" && @hand2 == "paper"
-      return "#{@hand1.upcase} beats #{@hand2.upcase}!"
-    elsif @hand1 == "paper" && @hand2 == "scissors"
-      return "#{@hand2.upcase} beats #{@hand1.upcase}!"
-    elsif @hand1 == "scissors" && @hand2 == "rock"
-      return "#{@hand2.upcase} beats #{@hand1.upcase}!"
-    elsif @hand1 == "rock" && @hand2 == "scissors"
-      return "#{@hand1.upcase} beats #{@hand2.upcase}!"
-    elsif @hand1 == "rock" && @hand2 == "paper"
-      return "#{@hand2.upcase} beats #{@hand1.upcase}!"
-    elsif @hand1 == "paper" && @hand2 == "rock"
-      return "#{@hand1.upcase} beats #{@hand2.upcase}!"
-    elsif @hand1 == "paper" && @hand2 == "paper"
-      return "Draw!"
-    elsif @hand1 == "rock" && @hand2 == "rock"
-      return "Draw!"
-    elsif @hand1 == "scissors" && @hand2 == "scissors"
-      return "Draw!"
-    end
+    return result1 if @hand1 == "scissors" && @hand2 == "paper"
+    return result1 if @hand1 == "paper" && @hand2 == "scissors"
+    return result3 if @hand1 == "scissors" && @hand2 == "rock"
+    return result3 if @hand1 == "rock" && @hand2 == "scissors"
+    return result2 if @hand1 == "rock" && @hand2 == "paper"
+    return result2 if @hand1 == "paper" && @hand2 == "rock"
+    return "Draw!" if @hand1 == "paper" && @hand2 == "paper"
+    return "Draw!" if @hand1 == "rock" && @hand2 == "rock"
+    return "Draw!" if @hand1 == "scissors" && @hand2 == "scissors"
 
   end
 
