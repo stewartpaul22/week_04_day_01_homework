56a95416d783be77abf130d3a51494d7b7a90941
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-12 22:06:37 +0000

    Updated tests for uppercase messages

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -5,32 +5,32 @@ class TestGame < Minitest::Test
   #1
   def test_scissors_plays_paper
     game = Game.new("scissors", "paper")
-    assert_equal("scissors beats paper!", game.play())
+    assert_equal("SCISSORS beats PAPER!", game.play())
   end
   #2
   def test_paper_plays_scissors
     game = Game.new("paper", "scissors")
-    assert_equal("scissors beats paper!", game.play())
+    assert_equal("SCISSORS beats PAPER!", game.play())
   end
   #3
   def test_scissors_plays_rock
     game = Game.new("scissors", "rock")
-    assert_equal("rock beats scissors!", game.play())
+    assert_equal("ROCK beats SCISSORS!", game.play())
   end
   #4
   def test_rock_plays_scissors
     game = Game.new("rock", "scissors")
-    assert_equal("rock beats scissors!", game.play())
+    assert_equal("ROCK beats SCISSORS!", game.play())
   end
   #5
   def test_rock_plays_paper
     game = Game.new("rock", "paper")
-    assert_equal("paper beats rock!", game.play())
+    assert_equal("PAPER beats ROCK!", game.play())
   end
   #6
   def test_paper_plays_rock
     game = Game.new("paper", "rock")
-    assert_equal("paper beats rock!", game.play())
+    assert_equal("PAPER beats ROCK!", game.play())
   end
   #7
   def test_paper_plays_paper
